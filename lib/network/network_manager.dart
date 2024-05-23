b3702c100265a30e9da7617f911971e30fce9841
appdevlopment <diptakumardas57@gmail.com>
2024-05-23 19:05:11 +0600

    api

---
+++ lib/network/network_manager.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:e_traceability_beta/network/requestModel/login_request.dart';
+import 'package:e_traceability_beta/network/response_model/login_response.dart';
+
+import '../api_constant/api_constant.dart';
+import 'package:http/http.dart' as http;
+
+
+LoginResponse loginResponse = LoginResponse();
+class NetworkManager {
+
+
+
+
+  Future<LoginResponse> logIn(LoginRequest request) async {
+    String url = AppConstant().baseUrl + AppConstant().loginUrl;
+    print(url);
+    print(jsonEncode(request));
+    final response = await http.post(
+      Uri.parse(url),
+      body: jsonEncode(request),
+      headers: {'Content-Type': 'application/json'},
+    );
+    print(response.body);
+    if (response.statusCode == 200) {
+      print(response.body);
+      final body = jsonDecode(response.body);
+      loginResponse = LoginResponse.fromJson(body);
+      return LoginResponse.fromJson(body);
+    }
+    else{
+      final body = jsonDecode(response.body);
+      throw Exception(body["encoded"]["error"]["errMsg"]);}
+  }
+
+}
